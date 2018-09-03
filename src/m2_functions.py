1d3afe496b5c0f8bf37a2be3b4ef06b3ed0099d4
jiangh2 <Ydong6688@>
2018-09-03 16:34:37 -0400

    session 2

--- src/m2_functions.py
+++ src/m2_functions.py
@@ -96,7 +96,7 @@ def function4(color,    thick):
 
 ###############################################################################
 #
-# TODO: 5.
+# DONE: 5.
 #   COMMIT-and-PUSH your work (after changing this TO-DO to DONE).
 #
 #   As a reminder, here is how you should do so:
